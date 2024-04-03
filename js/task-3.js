91989f3d65c549aa9d6d7c0857d64d5ac31127a6
Mitka92 <134132348+Mitka92@users.noreply.github.com>
2024-04-03 18:32:50 +0300

    refactoring task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,6 @@
 "use strict";
 function getElementWidth(content, padding, border) {
-  const contentWidth = parseFloat(content);
-  const paddingWidth = parseFloat(padding) * 2;
-  const borderWidth = parseFloat(border) * 2;
-  return contentWidth + paddingWidth + borderWidth;
+  return parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
